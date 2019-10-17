63bed9119200d5a61a36181b95509a7671af41ec
Tanguy D'auria$ <tanguy.d-auria@epitech.eu>
2019-10-17 18:01:13 -0400

    Added 404 handling + updated serve

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './Components/Login/Login';
 import LandingPage from './Components/Landing/LandingPage';
@@ -9,26 +9,23 @@ import Profile from './Components/Profile/Profile';
 import Prescription from './Components/Prescription/View';
 import Settings from './Components/Settings/Settings';
 import Map from './Components/Map/Map';
+import NotFound from './Components/Shared/404/NotFound';
 
 class App extends Component {
     render() {
         return (
             <BrowserRouter>
                 <div>
-                    <Route exact={true} path='/' render={() => (
-                        <div>
-                            <LandingPage />
-                        </div>
-                    )}/>
+                  <Switch>
+                    <Route exact={true} path='/' component={LandingPage}/>
                     <Route exact={true} path='/login' component={Login}/>
-                     {
-                       // <Route exact={true} path='/register' component={Register}/>
-                     }
                     <Route exact={true} path='/chat' component={Chat}/>
                     <Route exact={true} path='/map' component={Map}/>
                     <Route exact={true} path='/profile' component={Profile}/>
                     <Route exact={true} path='/prescription' component={Prescription}/>
                     <Route exact={true} path='/profile/settings' component={Settings}/>
+                    <Route component={NotFound} />
+                  </Switch>
                 </div>
             </BrowserRouter>
         );
