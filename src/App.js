a29945c98479145374637ac4652f49c454b1048c
Leandre <leandre.blanchard@epitech.eu>
2019-12-11 21:02:57 +0900

    [dev] addchild chat component and view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './Components/Login/Login';
 import LandingPage from './Components/Landing/LandingPage';
 import Register from './Components/Register/Register';
-import Chat from './Components/Chat/View';
+import Chat from './Components/Chat/Chat';
 import Profile from './Components/Profile/Profile';
 import Prescription from './Components/Prescription/View';
 import Settings from './Components/Settings/Settings';
@@ -12,25 +12,25 @@ import Map from './Components/Map/Map';
 import NotFound from './Components/Shared/404/NotFound';
 
 class App extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <div>
-                  <Switch>
-                    <Route exact={true} path='/' component={LandingPage}/>
-                    <Route exact={true} path='/login' component={Login}/>
-                    <Route exact={true} path='/register' component={Register}/>
-                    <Route exact={true} path='/chat' component={Chat}/>
-                    <Route exact={true} path='/map' component={Map}/>
-                    <Route exact={true} path='/profile' component={Profile}/>
-                    <Route exact={true} path='/prescription' component={Prescription}/>
-                    <Route exact={true} path='/profile/settings' component={Settings}/>
-                    <Route component={NotFound} />
-                  </Switch>
-                </div>
-            </BrowserRouter>
-        );
-    }
+	render() {
+		return (
+			<BrowserRouter>
+				<div>
+					<Switch>
+						<Route exact={true} path='/' component={LandingPage} />
+						<Route exact={true} path='/login' component={Login} />
+						<Route exact={true} path='/register' component={Register} />
+						<Route exact={true} path='/chat' component={Chat} />
+						<Route exact={true} path='/map' component={Map} />
+						<Route exact={true} path='/profile' component={Profile} />
+						<Route exact={true} path='/prescription' component={Prescription} />
+						<Route exact={true} path='/profile/settings' component={Settings} />
+						<Route component={NotFound} />
+					</Switch>
+				</div>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
