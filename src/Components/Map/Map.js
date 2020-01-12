6f3371db30b1f13604a89684e404d47c100230b5
Camille ARBAULT <camille.arbault@epitech.eu>
2020-01-12 16:41:15 +0100

    map redo

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,170 +1,90 @@
-import React, { Component } from 'react';
-import {
-    Map, TileLayer, ZoomControl, Marker
-} from 'react-leaflet';
-import MapRouting from "./MapRouting";
-import LocateControl from './Shared/LocateUser';
-import DiscreteSlider from './Shared/Slider';
-import HospitalPopup from "./Shared/HospitalPopup"
-import Search from './Shared/Search';
-import { FaAmbulance } from 'react-icons/fa';
-import { renderToStaticMarkup } from 'react-dom/server';
-import { divIcon } from 'leaflet';
-import classes from "./Map.scss"
+import React, {useState} from 'react';
+import {GoogleMap, Marker, Autocomplete } from '@react-google-maps/api'
+import {mapOptions} from "./MapOptions";
+import hospitalIcon from "../../Images/map_marker.png"
 
-class LeafletMap extends Component {
+function Map() {
 
-    constructor(props) {
-        super(props);
-        this.mapRef = React.createRef();
-        this.state = {
-            //default lat and lng are Paris coordinates
-            lat: 48.8566,
-            lng: 2.3522,
-            markerLat: 0,
-            markerLng: 0,
-            zoom: 5,
-            radius: 0.03,
-            isMapInit: false,
-            hospitalSelected: false,
-            //list of hospitals
-            nearestHospitals: [],
-            hospitalMarkers: [],
-            userPos: null,
-            //in case userPos != route start pos
-            routeFrom: [46.5, 2.618787],
-            routeTo: [48.85412, 2.4065929]
+    const [center] = useState({ lat: 48.8566, lng: 2.3522});
+    //const [mapRef, setMapRef] = useState(null);
+    const [userPos] = useState({ lat: 48.8566, lng: 2.3522});
+    const [searchRadius] = useState(1500);
+    const [zoom] = useState(15);
+    const [hospitals, setHospitals] = useState([]);
+    const [hospitalMarkers, setHospitalMarkers] = useState(null);
+
+    const findNearestHospitals = map => {
+        let service = new window.google.maps.places.PlacesService(map);
+        let pos = new window.google.maps.LatLng(userPos.lat, userPos.lng);
+        var request = {
+            location: pos,
+            radius: searchRadius,
+            type: 'hospital'
         };
-        this.addMarker = this.addMarker.bind(this);
-    }
-
-    getNearestHospitals() {
-        this.setState({ stop: true })
-        let url = this.state.userPos ? "https://www.kwili.fr:8080/urgences?radius=" + this.state.radius + "&lat=" + this.state.userPos.lat
-                + "&long=" + this.state.userPos.lng : "https://www.kwili.fr:8080/urgences?radius=" + this.state.radius + "&lat=" + this.state.lat
-                + "&long=" + this.state.lng;
-        fetch(url)
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    this.setState({nearestHospitals: result.msg});
-                    if (result.msg !== undefined && result.msg[0] !== undefined && result.msg[0].geo !== undefined) {
-                        this.createHospitalMarkers();
-                        this.setState({routeTo: [result.msg[0].geo[1], result.msg[0].geo[0]]});
-                        this.setState({selectedHospital: true});
-                    }
-                },
-                (error) => {
-                    this.setState({error});
+        service.nearbySearch(request, (results, status) => {
+            if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+                let markerList = [];
+                setHospitals(results);
+                console.log(hospitals);
+                for (let i = 0; i < results.length; i++) {
+                    markerList.push(<Marker
+                        // label={results[i].name}
+                        position={results[i].geometry.location}
+                        icon={hospitalIcon}
+                    />);
                 }
-            )
-    }
-
-    componentDidMount() {
-        document.title = 'Kwili | Urgences à proximité';
-        if (this.state.isMapInit === false) {
-            this.setState({
-                isMapInit: true,
-                stop: false
-            });
-            this.getNearestHospitals();
-        }
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if ((this.state.userPos !== prevState.userPos)) {
-            this.getNearestHospitals();
-        } else if ((this.state.radius !== prevState.radius)) {
-            this.getNearestHospitals();
-        }
-    }
-
-    updateUserPosition(pos) {
-        this.setState({userPos: pos});
-    }
-
-    addMarker(x, y) {
-        this.setState({ markerLat: x, markerLng: y });
-    }
-
-    saveMap = (map) => {
-        this.map = map;
+                setHospitalMarkers(markerList);
+            }
+        })
     };
 
-    onClick = (pos) => {
-        this.child.updateHospital(pos);// do stuff
+    const loadHandler = map => {
+
+        //Store a reference to the google map instance in state
+        //setMapRef(map);
+
+        //fetch nearest hospitals
+        findNearestHospitals(map);
+
     };
 
-    setRadius(val) {
-        this.setState({radius: val});
-    }
+    const renderMap = () => {
 
-    createHospitalMarkers() {
-        if (this.state.nearestHospitals !== []) {
-            let markers = [];
-            let self = this;
-            const iconMarkup = renderToStaticMarkup(<FaAmbulance className={classes.hospitalIcon}/>);
-            const customIcon = divIcon({
-                html: iconMarkup,
-                className: 'hospitalIcon'
-            });
-
-            this.state.nearestHospitals.forEach(function (hospital) {
-                markers.push(
-                    <Marker position={[hospital.geo[1], hospital.geo[0]]} key={hospital.id} icon={customIcon}
-                            onClick={() => self.onClick([hospital.geo[1], hospital.geo[0]])}>
-                        <HospitalPopup name={hospital.n}/>
-                    </Marker>
-                );
-            });
-            this.setState({hospitalSelected: true, hospitalMarkers: markers});
-        }
-    }
-
-    render() {
-        const position = [this.state.lat, this.state.lng];
-        return (
-            <div>
-                <Map
-                    zoomControl={false}
-                    center={position}
-                    zoom={this.state.zoom}
-                    style={{ height: '90vh' }}
-                    minZoom={2}
-                    max Zoom={19}
-                    ref={this.saveMap}
-                    onLocationFound={event => {this.updateUserPosition(event.latlng)}}
-                    onLocationError={() => this.setState({userPos: position})}
-                >
-                    <TileLayer
-                        attribution='&amp;copy <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &amp;copy <a href="https://carto.com/attributions">CARTO</a>'
-                        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        return <GoogleMap
+            options={mapOptions}
+            zoom={zoom}
+            center={center}
+            mapContainerStyle={{
+                height: "100vh",
+                width: "100vwh"
+            }}
+            onLoad={map => loadHandler(map)}
+        >
+                <Autocomplete>
+                    <input
+                        type="text"
+                        placeholder="Rechercher une adresse"
+                        style={{
+                            boxSizing: `border-box`,
+                            border: `1px solid transparent`,
+                            width: `240px`,
+                            height: `32px`,
+                            padding: `0 12px`,
+                            borderRadius: `10px`,
+                            boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                            fontSize: `14px`,
+                            outline: `none`,
+                            textOverflow: `ellipses`,
+                            position: "fixed",
+                            margin: "30px"
+                        }}
                     />
-                    {(this.state.markerLat !== 0 && this.state.markerLng !== 0) ?
-                        (
-                            <Marker position={[this.state.markerLat, this.state.markerLng]} />
-                        ) : null
-                    }
-                    <ZoomControl position="bottomleft" />
-                    <Search />
-                    <LocateControl startDirectly/>
-                    {
-                        //only display routing when user is geolocated and an hospital is clicked
-                        //or use default coordinates if user refused geolocation
-                        this.state.isMapInit && this.state.userPos && this.state.selectedHospital && <MapRouting
-                            routeFrom={this.state.userPos}
-                            routeTo={this.state.routeTo}
-                            map={this.map}
-                            updateUserPos={(pos) => this.setState({userPos: pos})}
-                            onRef={ref => (this.child = ref)}
-                        />
-                    }
-                    {this.state.hospitalMarkers}
-                </Map>
-                <DiscreteSlider setRadius={(val) => this.setState({radius: val})}/>
-            </div>
-        );
-    }
+                </Autocomplete>
+            {hospitalMarkers}
+        </GoogleMap>
+    };
+
+    return renderMap();
 }
 
-export default LeafletMap;
+export default Map;
---
+++ src/Components/Map/MapOptions.js
@@ -0,0 +1,104 @@
+export const mapOptions = {
+    streetViewControl: false,
+    zoomControl: true,
+    mapTypeControl: false,
+    scaleControl: true,
+    rotateControl: false,
+    fullscreenControl: false,
+    styles: [
+        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.stroke', stylers: [{visibility: "off"}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#58a5f0'}]},
+        {
+            featureType: 'administrative.locality',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#58a5f0'}]
+        },
+        {
+            featureType: "administrative.neighborhood",
+            elementType: "labels",
+            stylers: [
+                {
+                    visibility: "off"
+                }
+            ]
+        },
+        {
+            featureType: "poi",
+            stylers: [
+                {visibility: "off"}
+            ]
+        },
+        {
+            featureType: 'poi.medical',
+            stylers: [
+                {
+                    visibility: 'on',
+                }]
+        },
+        {
+            featureType: 'poi.medical',
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#EBEBEB",
+                }]
+        },
+        {
+            featureType: 'poi.medical',
+            elementType: "labels.text.stroke",
+            stylers: [
+                {
+                    visibility: "off",
+                }]
+        },
+        {
+            featureType: 'road',
+            elementType: 'labels',
+            stylers: [
+                {
+                    visibility: "off"
+                }]
+        },
+        {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{color: '#3A505D'}]
+        },
+        {
+            featureType: 'road',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#4D6A7A'}]
+        },
+        {
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{color: '#617A8A'}]
+        },
+        {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#1f2835'}]
+        },
+        {
+            featureType: 'transit',
+            elementType: 'geometry',
+            stylers: [{color: '#3a4762'}]
+        },
+        {
+            featureType: 'transit.station',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#58a5f0'}]
+        },
+        {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [{color: '#4e6d70'}]
+        },
+        {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#58a5f0'}]
+        },
+    ]
+};
