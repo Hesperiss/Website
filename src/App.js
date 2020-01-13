0ac5e3458daa70560fd0f45828b2e26c063e2714
Tanguy D'auria$ <tanguy.d-auria@epitech.eu>
2020-01-12 21:01:51 -0500

    modified app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import LandingPage from './Components/Landing/LandingPage';
 import Register from './Components/Register/Register';
 import Chat from './Components/Chat/Chat';
 import Profile from './Components/Profile/Profile';
-import Prescription from './Components/Prescription/View';
+//import Prescription from './Components/Prescription/View';
 import Settings from './Components/Settings/Settings';
 import Map from './Components/Map/Map';
 import NotFound from './Components/Shared/404/NotFound';
@@ -25,7 +25,6 @@ class App extends Component {
 						<Route exact={true} path='/chat' component={Chat} />
 						<Route exact={true} path='/map' component={Map} />
 						<Route exact={true} path='/profile' component={Profile} />
-						<Route exact={true} path='/prescription' component={Prescription} />
 						<Route exact={true} path='/profile/settings' component={Settings} />
             <Route exact={true} path='/test' component={() => <CheckLogin path='/login'/>}/>
 						<Route component={NotFound} />
