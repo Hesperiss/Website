eb7e94b56d0b66d341a940f4e7db03dcd7351b9c
Camille ARBAULT <camille.arbault@epitech.eu>
2020-02-23 01:47:14 +0100

    intro + some anims
    background need to be fixed

--- src/Components/Landing/LandingPage.js
+++ src/Components/Landing/LandingPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import NavBar from "./Components/Navbar";
 import BlueCurve from "./Images/blue_curve.svg"
+import background from "./Images/Background.png"
+import phoneDemo from "./Images/phone_section1.png"
 
 class LandingPage extends React.Component {
 
@@ -17,6 +19,18 @@ class LandingPage extends React.Component {
 		return (
 			<div className={"root"}>
 				<NavBar />
+
+				<div className={"introSection"}>
+					<img src={phoneDemo} alt={"kwili webApp demo"}/>
+					<div className={"introText"}>
+						<h1>KWILI</h1>
+						<p>Un moyen simple et rapide pour accéder aux urgences.<br/>
+						Une première prise en charge en ligne.</p>
+						<input type="submit" value="En savoir plus"/>
+					</div>
+				</div>
+
+				<img src={background} alt={"doctor office"} className={"background"}/>
 				<img src={BlueCurve} alt={"blue background"} className={"blueCurve"}/>
 			</div>
 		);
