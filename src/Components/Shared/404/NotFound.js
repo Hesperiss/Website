18440439fffebacb6fe9a0e7c33d2386cbc8e85f
Hesperis <hessperis@gmail.com>
2020-04-03 16:29:56 +0200

    [fix] fix 404 error css

--- src/Components/Shared/404/NotFound.js
+++ src/Components/Shared/404/NotFound.js
@@ -2,24 +2,20 @@ import React from 'react';
 
 import NavBarLanding from '../../Landing/Components/Navbar';
 
-import './css/style.css';
+import './css/NotFound.scss';
 
 const Error = () => {
-  return (
-    <div style={{ backgroundColor: 'white'}}>
-      <NavBarLanding/>
-      <div id='notfound'>
-        <div class='notfound'>
-          <div class='notfound404'>
-            <h1>Oops!</h1>
-          </div>
-          <h2>404 - Page not found</h2>
-          <p>La page que vous recherchez n'est pas disponible, elle a peut-être été modifiée ou retirée.</p>
-          <a href="/">Accueil</a>
+    return (
+        <div className={"notFoundWrapper"}>
+            <NavBarLanding/>
+            <div className={"notFound"}>
+                <h1>Oops!</h1>
+                <h2>404 - Page not found</h2>
+                <p>La page que vous recherchez n'est pas disponible, elle a peut-être été modifiée ou retirée.</p>
+                <a href="/">Accueil</a>
+            </div>
         </div>
-      </div>
-    </div>
-  )
-}
+    )
+};
 
 export default Error;
