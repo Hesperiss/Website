85e7e35024cf5be8335eb31b68b99f8884f54c4e
Hesperis <hessperis@gmail.com>
2020-04-03 17:39:19 +0200

    [fix] minor css changes

--- src/Components/Shared/404/NotFound.js
+++ src/Components/Shared/404/NotFound.js
@@ -8,12 +8,20 @@ const Error = () => {
     return (
         <div className={"notFoundWrapper"}>
             <NavBarLanding/>
+
+            <div className={"blueCurveWrapper"} >
+                <svg viewBox="0 0 500 150" className={"blueCurve"} preserveAspectRatio="none">
+                    <path d="M-2.54,109.03 C214.73,168.25 308.96,67.59 507.62,138.64 L500.00,0.00 L0.00,0.00 Z"></path>
+                </svg>
+            </div>
+
             <div className={"notFound"}>
                 <h1>Oops!</h1>
                 <h2>404 - Page not found</h2>
                 <p>La page que vous recherchez n'est pas disponible, elle a peut-être été modifiée ou retirée.</p>
                 <a href="/">Accueil</a>
             </div>
+
         </div>
     )
 };
