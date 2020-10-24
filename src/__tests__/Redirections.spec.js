0d22a0c28a558533479883aa27112a978a384abf
Tanguy D'auria$ <tanguy.d-auria@epitech.eu>
2020-10-24 12:22:30 +0200

    [FIX] Updated all dependencies, removec all warning messages. Please update to node>=v14

--- src/__tests__/Redirections.spec.js
+++ src/__tests__/Redirections.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
-import { mount, shallow, configure } from 'enzyme';
+import { shallow, configure } from 'enzyme';
 import Home from '../Components/Home/Home';
 
 configure({adapter: new Adapter()});
